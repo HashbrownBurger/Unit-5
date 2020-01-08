2cb3bf30b2df9ea1f38e75e60f35d2e9cd6ba576
HashbrownBurger <54951157+HashbrownBurger@users.noreply.github.com>
2020-01-08 09:27:28 -0800

    LJWEN

--- src/num.java
+++ src/num.java
@@ -1,3 +1,23 @@
 public class num {
+    private int value;
 
+    public num(int value){
+        this.value = value;
+    }
+
+    public int getvalue(){
+        return value;
+    }
+
+    public void setvalue(int value){
+        this.value = value;
+    }
+    public String toString(){
+        return "" + value;
+    }
+
+    public boolean isEqual(num otherNumber){
+        return this.value == otherNumber.value;
+        }
+    }
 }
